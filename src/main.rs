8c76707cc9127e1f5815a38e620e339f2b52f3b5
Tshepang Lekhonkhobe <tshepang@gmail.com>
2018-10-14 14:35:30 +0200

    moar

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,32 @@
 use std::{fs, io};
 
+fn type_elision() {
+    let value: Vec<u8> = (1_u8..=3).collect();
+    println!("explicit: {:?}", value);
+    let value: Vec<_> = (1..=3).collect();
+    println!("elided: {:?}", value);
+}
+
+fn type_elision_shorthand() {
+    struct Number(i32);
+    impl Number {
+        fn increase(self: &mut Self) {
+            self.0 += 1;
+        }
+        fn increase_elided(&mut self) {
+            self.0 += 1;
+        }
+    }
+    let mut number = Number(0);
+    number.increase();
+    println!("explicit increment: {}", number.0);
+    number.increase_elided();
+    println!("elided increment: {}", number.0);
+    println!("'&mut self' ~-> 'self: &mut Self'");
+    println!("'&self' ~-> 'self: &Self'");
+    println!("'self' ~-> 'self: Self'");
+}
+
 fn methods() {
     mod some_module {
         pub struct MaxThree {
@@ -185,9 +212,74 @@ fn type_inference_and_coercion() {
     println!("no inference or coercion: {:#X}", -foo);
 }
 
+fn deref_coercion() {
+    let vec = vec![1, 2, 3];
+    let arr = [1, 2, 3];
+    let equal = vec == arr;
+    assert!(equal);
+}
+
 fn hidden_code() {
-    extern crate std;
-    use std::prelude::v1::*;
+    println!("
+automatically inserted:
+ #[macro_use]
+ extern crate std;
+ use std::prelude::v1::*;
+    ");
+    println!("\
+Also done for us automatically:
+- eh_personality: runs destructors in case of panic (stack unwinding)
+- panic_handler: a function to be called in case of panic
+- start: setup function called before main()
+    ");
+}
+
+fn if_let() {
+    let maybe = (1..3).next();
+    println!("version 0");
+    match maybe {
+        Some(value) => println!("{:?}", value),
+        None => (),
+    };
+    println!("version 0 (if let)");
+    if let Some(value) = maybe {
+        println!("{:?}", value);
+    };
+}
+
+fn pretty_imports() {
+    println!("
+to avoid repetition, one can do this:
+ use std;:path::{{self, Path, PathBuf}};
+compare that to this:
+ use std::path;
+ use std::path::Path;
+ use std::path::PathBuf;
+    ");
+}
+
+fn implicit_return() {
+    fn explicit_return() -> i32 {
+        if true {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+    fn implicit_return() -> i32 {
+        if true {
+            0
+        } else {
+            1
+        }
+    }
+    fn _implicit_return_compact() -> i32 {
+        if true { 0 } else { 1 }
+    }
+    fn _explicit_return_compact() -> i32 {
+        if true { return 0; } else { return 1; }
+    }
+    assert_eq!(explicit_return(), implicit_return());
 }
 
 fn run() -> io::Result<()> {
@@ -200,7 +292,14 @@ fn run() -> io::Result<()> {
     derive();
     lifetime_elision();
     type_inference_and_coercion();
+    deref_coercion();
     hidden_code();
+    if_let();
+    type_elision();
+    type_elision_shorthand();
+    pretty_imports();
+    implicit_return();
+    deref_coercion();
     Ok(())
 }
 
